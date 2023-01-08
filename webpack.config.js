17ef3c9d729dabef0e89b7d656b26f48d47bd9cb
ori <93719767+oriodev@users.noreply.github.com>
2023-01-08 23:54:48 +0000

    added basic html skeleton

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: "development",
+  module: {
+      rules: [
+        {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: 'asset/resource',
+        },
+      ]
+  }
+};
