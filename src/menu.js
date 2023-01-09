9e7161f59e48bd2cad98dfdfeb2046411ba24e37
ori <93719767+oriodev@users.noreply.github.com>
2023-01-09 17:12:48 +0000

    render menu page via js

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,44 @@
-const renderMenu1 = () => {
-    console.log("hi");
+const renderMenu = () => {
+    const content = document.getElementById("content");
+
+    // CREATE CENTER
+    const center = document.createElement('div');
+    center.setAttribute("class", "center");
+    content.appendChild(center);
+
+    // CREATE MAIN SECT
+    const mainSect = document.createElement('div');
+    mainSect.setAttribute("class", "mainSect");
+    center.appendChild(mainSect);
+
+    // CREATE TITLE
+    const menuTitle = document.createElement('h2');
+    menuTitle.setAttribute("class", "menuTitle");
+    menuTitle.textContent = "MENU ONE";
+    mainSect.appendChild(menuTitle);
+
+    // CREATE CIRCLES
+    const menuCircles = document.createElement('div');
+    menuCircles.setAttribute("class", "menuCircles");
+    center.appendChild(menuCircles);
+
+    const circle1 = document.createElement('div');
+    circle1.textContent = "-";
+    menuCircles.appendChild(circle1);
+
+    const circle2 = document.createElement('div');
+    circle2.textContent = "-";
+    menuCircles.appendChild(circle2);
+
+    const circle3 = document.createElement('div');
+    circle3.textContent = "-";
+    menuCircles.appendChild(circle3);
+
 }
 
-export { renderMenu1 }
+const renderMenu2 = () => {
+
+    console.log("hiyaa");
+}
+
+export { renderMenu, renderMenu2}
