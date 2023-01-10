133931abbcda974956c48c3c0dedebc9a5a9663f
ori <93719767+oriodev@users.noreply.github.com>
2023-01-10 20:33:32 +0000

    changing compile path

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,10 @@ module.exports = {
             test: /\.(woff|woff2|eot|ttf|otf)$/i,
             type: 'asset/resource',
         },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
       ]
   }
 };
